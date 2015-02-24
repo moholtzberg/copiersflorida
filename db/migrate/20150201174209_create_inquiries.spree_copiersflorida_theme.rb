0fb30c30699a1ec44ade6a701bcfb4a2d8d5bea8
Moshe Holtzberg <moholtzberg@gmail.com>
2015-02-24 14:17:41 -0500

    updated gem spree_copiersflorida_theme

---
+++ db/migrate/20150201174209_create_inquiries.spree_copiersflorida_theme.rb
@@ -0,0 +1,18 @@
+# This migration comes from spree_copiersflorida_theme (originally 20150126220724)
+class CreateInquiries < ActiveRecord::Migration
+  def self.up
+    create_table :inquiries do |t|
+      t.integer :order_id
+      t.string  :name
+      t.string  :email
+      t.text  :message
+      t.boolean :viewd
+      t.boolean :complete
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :inquiries
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120162420) do
+ActiveRecord::Schema.define(version: 20150201174209) do
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 20150120162420) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "inquiries", force: true do |t|
+    t.integer  "order_id"
+    t.string   "name"
+    t.string   "email"
+    t.text     "message"
+    t.boolean  "viewd"
+    t.boolean  "complete"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "spree_addresses", force: true do |t|
     t.string   "firstname"
