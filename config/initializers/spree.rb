f3de239a9307616f26c06824d3899c731b3456c3
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-02 16:02:37 -0500

    moved s3 config to application.rb

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -7,25 +7,22 @@
 # config.setting_name = 'new value'
 Spree.config do |config|
   attachment_config = {
-    s3_credentials: {
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      bucket: ENV['S3_BUCKET_NAME'],
-    },
+
     storage: :s3,
     s3_headers: { 
       "Cache-Control" => "max-age=31557600" 
     },
     s3_protocol:    "https",
     bucket:         ENV['S3_BUCKET_NAME'],
+
     styles: {
       mini:     "48x48>",
       small:    "120x120>",
       product:  "240x240>",
       large:    "600x600>"
     },
-    path:          ":rails_root/public/spree/images/:id/:style/:basename.:extension",
-    default_url:   "/spree/images/:id/:style/:basename.:extension",
+    path:          "/spree/:class/:id/:style/:basename.:extension",
+    default_url:   "/spree/:class/:id/:style/:basename.:extension",
     default_style: "product"
   }
   
