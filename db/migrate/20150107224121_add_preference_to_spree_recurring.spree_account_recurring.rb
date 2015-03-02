b6a7addb802317f67912c7ad2fbfc2bebfce3d80
Moshe Holtzberg <moholtzberg@gmail.com>
2015-03-02 14:03:34 -0500

    removed migrations from spree_account_recurring

--- db/migrate/20150107224121_add_preference_to_spree_recurring.spree_account_recurring.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from spree_account_recurring (originally 20141218173349)
-class AddPreferenceToSpreeRecurring < ActiveRecord::Migration
-  def change
-    add_column :spree_recurrings, :preferences, :text
-  end
-end
