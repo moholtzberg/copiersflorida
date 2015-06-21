6948bea1f29c6dd5477879fea0edb0ffea4ff9a4
Moshe Holtzberg <moholtzberg@gmail.com>
2015-06-21 13:03:32 -0400

    debug unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -21,9 +21,13 @@
 #     ActiveRecord::Base.establish_connection
 # end
 
+
+# https://www.digitalocean.com/community/tutorials/how-to-deploy-a-rails-app-with-unicorn-and-nginx-on-ubuntu-14-04
 # set path to application
 app_dir = File.expand_path("../..", __FILE__)
+puts "---------------------------------------------#{app_dir}"
 shared_dir = "#{app_dir}/shared"
+puts "---------------------------------------------#{shared_dir}"
 working_directory app_dir
 
 
