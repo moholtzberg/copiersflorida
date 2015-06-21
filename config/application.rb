4787118fa0cd0d679bc1dd725e83638edfc4a6a1
Moshe Holtzberg <moholtzberg@gmail.com>
2015-06-20 23:43:47 -0400

    support app_secrets.yml

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+secret_file = "../../shared/app_secrets.yml"
+SECRET = File.exists?(secret_file) ? YAML.load_file(secret_file) : {}
+
 module Copiersflorida
   class Application < Rails::Application
     
