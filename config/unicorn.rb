4443054a2fbf4bece14d716db076529d1bae10a3
Moshe Holtzberg <moholtzberg@gmail.com>
2015-06-21 13:22:19 -0400

    unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -22,23 +22,63 @@
 # https://www.digitalocean.com/community/tutorials/how-to-deploy-a-rails-app-with-unicorn-and-nginx-on-ubuntu-14-04
 # set path to application
 # app_dir = File.expand_path("../..", __FILE__)
-app_dir = "/home/spree/copiersflorida/current"
-puts "---------------------------------------------#{app_dir}"
-shared_dir = "/home/spree/copiersflorida/shared"
-puts "---------------------------------------------#{shared_dir}"
-working_directory app_dir
+# app_dir = "/home/spree/copiersflorida/current"
+# puts "---------------------------------------------#{app_dir}"
+# shared_dir = "/home/spree/copiersflorida/shared"
+# puts "---------------------------------------------#{shared_dir}"
+# working_directory app_dir
+# 
+# # Set unicorn options
+# worker_processes 2
+# preload_app true
+# timeout 30
+# 
+# # Set up socket location
+# listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+# 
+# # Logging
+# stderr_path "#{shared_dir}/log/unicorn.stderr.log"
+# stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+# 
+# # Set master PID location
+# pid "#{shared_dir}/pids/unicorn.pid"
 
-# Set unicorn options
+root = "/home/spree/copiersflorida/current"
+working_directory root
+
+pid "#{root}/tmp/pids/unicorn.pid"
+
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
 worker_processes 2
-preload_app true
 timeout 30
+preload_app true
 
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
+listen '/tmp/unicorn.spui.sock', backlog: 64
 
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+  puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+  Process.kill 'QUIT', Process.pid
+  end
 
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
+  defined?(ActiveRecord::Base) and
+  ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+  puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+  ActiveRecord::Base.establish_connection
+end
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV['BUNDLE_GEMFILE'] = File.join(root, 'Gemfile')
+end
+
