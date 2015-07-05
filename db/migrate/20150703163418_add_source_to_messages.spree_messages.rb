93f8b4b2b1bea452beefa7b55fc7934fe7f7f4d2
Moshe Holtzberg <moholtzberg@gmail.com>
2015-07-05 03:02:32 -0400

    fix migration from spree_messages

--- db/migrate/20150703163418_add_source_to_messages.spree_messages.rb
+++ db/migrate/20150703163418_add_source_to_messages.spree_messages.rb
@@ -3,7 +3,7 @@ class AddSourceToMessages < ActiveRecord::Migration
   def change
     add_column :messages, :source, :string
     add_column :messages, :taxon_id, :integer
-    change_column :messages, :viewed, :viewd
+    rename_column :messages, :viewd, :viewed
     add_column :messages, :phone, :string
     add_column :messages, :vendor, :string
   end
